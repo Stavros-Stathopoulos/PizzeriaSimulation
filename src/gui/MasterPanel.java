515e0c99a8bbd43fb417fb7c57f32bd4d268ad49
Stavros Stathopoulos <38631315+Stavros-Stathopoulos@users.noreply.github.com>
2023-12-22 11:04:18 +0200

    gui improvement

--- src/gui/MasterPanel.java
+++ src/gui/MasterPanel.java
@@ -2,9 +2,11 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class MasterPanel extends JPanel {
-    
+    private Color backgroundColor = Color.darkGray;
     public MasterPanel(){
         this.setLayout(new BorderLayout());
         this.add(SimulationPanelNorth(), BorderLayout.NORTH);
@@ -14,7 +16,7 @@ public class MasterPanel extends JPanel {
     private JPanel SimulationPanelNorth() {
         JPanel simulationPanelNorth= new JPanel();
         simulationPanelNorth.setLayout(new GridLayout(1,0));
-        simulationPanelNorth.setBackground(Color.black);
+        simulationPanelNorth.setBackground(backgroundColor);
         simulationPanelNorth.setPreferredSize(new Dimension(550, 100));
 
         JLabel label = new JLabel("Pizzeria Simulation");
@@ -31,7 +33,7 @@ public class MasterPanel extends JPanel {
     private JPanel SimulationPanelCenter() {
         JPanel simulationPanelCenter = new JPanel();
         simulationPanelCenter.setLayout(new GridLayout(4,2));
-        simulationPanelCenter.setBackground(Color.black);
+        simulationPanelCenter.setBackground(backgroundColor);
         simulationPanelCenter.setPreferredSize(new Dimension(550, 450));
 
         JLabel label = new JLabel("Simulation duration:");
@@ -60,15 +62,37 @@ public class MasterPanel extends JPanel {
         JTextField textField3 = getTextField("Enter Extra Delivery Time");
 
         JButton startSimulationButton = getButton(textField, textField2, textField3);
+        JPanel textField1p = new JPanel();
+        textField1p.setBackground(backgroundColor);
+        textField1p.setPreferredSize(new Dimension(100, 50));
+        textField1p.add(textField);
+
+        JPanel textField2p = new JPanel();
+        textField2p.setBackground(backgroundColor);
+        textField2p.setPreferredSize(new Dimension(100, 50));
+        textField2p.add(textField2);
+
+        JPanel textField3p = new JPanel();
+        textField3p.setBackground(backgroundColor);
+        textField3p.setPreferredSize(new Dimension(100, 50));
+        textField3p.add(textField3);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(backgroundColor);
+        buttonPanel.setPreferredSize(new Dimension(300, 80));
+        buttonPanel.add(startSimulationButton);
 
 
+        simulationPanelCenter.add(label,0);
         simulationPanelCenter.add(label);
-        simulationPanelCenter.add(textField);
-        simulationPanelCenter.add(label2);
-        simulationPanelCenter.add(textField2);
-        simulationPanelCenter.add(label3);
-        simulationPanelCenter.add(textField3);
-        simulationPanelCenter.add(startSimulationButton);
+        simulationPanelCenter.add(textField1p, 1);
+
+        simulationPanelCenter.add(label2, 0);
+        simulationPanelCenter.add(textField2p, 1);
+
+        simulationPanelCenter.add(label3, 0);
+        simulationPanelCenter.add(textField3p, 1);
+        simulationPanelCenter.add(buttonPanel);
 
         return simulationPanelCenter;
     }
@@ -76,7 +100,7 @@ public class MasterPanel extends JPanel {
     private JButton getButton(JTextField textField, JTextField textField2, JTextField textField3) {
         JButton startSimulationButton = new JButton("Start Simulation");
         startSimulationButton.setFont(new Font("Serif", Font.BOLD, 20));
-        startSimulationButton.setBackground(Color.BLACK);
+        startSimulationButton.setBackground(backgroundColor);
         startSimulationButton.setForeground(Color.WHITE);
         startSimulationButton.setBorder(BorderFactory.createLineBorder(Color.WHITE));
         startSimulationButton.setFocusPainted(false);
@@ -90,12 +114,31 @@ public class MasterPanel extends JPanel {
     }
 
     private JTextField getTextField(String str) {
-        JTextField textField3 = new JTextField(str, 10);
-        textField3.setFont(new Font("Serif", Font.BOLD, 20));
-        textField3.setBackground(Color.BLACK);
+        JTextField textField3 = new JTextField(str);
+        textField3.setPreferredSize(new Dimension(150, 40));
+        textField3.setFont(new Font("Serif", Font.BOLD, 10));
+        textField3.setBackground(backgroundColor);
         textField3.setForeground(Color.WHITE);
-        textField3.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+        textField3.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         textField3.setFocusable(true);
+        textField3.setAlignmentY(JTextField.CENTER_ALIGNMENT);
+        textField3.addFocusListener(new FocusListener() {
+                                        @Override
+                                        public void focusGained(FocusEvent e) {
+                                            // Clear the prompt text when the JTextField gains focus
+                                            if (textField3.getText().equals(str)) {
+                                                textField3.setText("");
+                                            }
+                                        }
+
+                                        @Override
+                                        public void focusLost(FocusEvent e) {
+                                            if (textField3.getText().isEmpty()) {
+                                                textField3.setText(str);
+                                            }
+                                        }
+
+                                    });
         textField3.setHorizontalAlignment(JTextField.CENTER);
         textField3.addActionListener(e -> {
             System.out.println("Start Simulation");
